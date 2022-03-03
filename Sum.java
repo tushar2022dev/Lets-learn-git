452ad33dbed63327fe63c1e4991508feec2c5430
Tushar <tushar.g7528@gmail.com>
2022-03-03 11:41:53 +0530

    sum java modifies

--- Sum.java
+++ Sum.java
@@ -3,7 +3,7 @@ public class Sum {
         int a = 8;
         int b = 5;
 
-        System.out.println("sum 0f the numbers is :" + (a + b));
+        System.out.println("sum of numbers is :" + (a + b));
 
     }
 
