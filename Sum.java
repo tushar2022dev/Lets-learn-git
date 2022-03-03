a711a749b5d0156dd32723c1cfd2192618ddfd5d
Tushar <tushar.g7528@gmail.com>
2022-03-03 11:34:22 +0530

    initial commit

---
+++ Sum.java
@@ -0,0 +1,10 @@
+public class Sum {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 5;
+
+        System.out.println("sum 0f the numbers is :" + (a + b));
+
+    }
+
+}
